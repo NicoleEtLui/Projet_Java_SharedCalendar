c979ed942afb999491cc9421a0e75c38468edd0e
NicoleEtLui <petit.martin125@gmail.com>
2016-11-30 14:30:22 +0100

    start console

--- src/view/ShaCalViewConsol.java
+++ src/view/ShaCalViewConsol.java
@@ -13,5 +13,18 @@ public class ShaCalViewConsol extends ShaCalView implements Observer {
 	
 	public ShaCalViewConsol(ShaCalModel model, ShaCalController controller) {
 		super(model, controller);
+		sc = new Scanner(System.in);
+		new Thread(new ReadInput()).start();
+	}
+	
+	
+	
+	private class ReadInput implements Runnable{
+		public void run() {
+			while(true){
+				//int newCTemp = sc.nextInt();
+				//controller.fixeDegresC(newCTemp);
+			}
+		}
 	}
 }
