5ff09071820c8a422661c32c888c021da938f078
RiddSann <nathan.vanderdonck@gmail.com>
2016-12-06 00:15:23 +0100

    Got rid of all useless tests depending on methods now taken care of by ShaCalModel. pre-release v1.0a

--- src/model/GroupTest.java
+++ src/model/GroupTest.java
@@ -5,66 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GroupTest {
-
+	
 	@Test
 	public void testToString() {
 		Group testGroup = new Group();
 		assertEquals(testGroup.toString(), "[Group] Id : \"" + testGroup.getGrId() + "\" Name : \"" + testGroup.getGrName() + "\" IsPublic : true"
 				+ "\nNumber of members : 0 | Number of events : 0");
+		Event event = new Event("event");
+		ShaCalModel.addEvent(String.valueOf(testGroup.getGrId()), event);
+		assertEquals(testGroup.toString(), "[Group] Id : \"" + testGroup.getGrId() + "\" Name : \"" + testGroup.getGrName() + "\" IsPublic : true"
+				+ "\nNumber of members : 0 | Number of events : 1");
 	}
 	
 	@Test
-	public void testEquals() {
+	public void equals() {
 		Group testGroup = new Group();
 		assertTrue(testGroup.equals(testGroup));
 	}
-	
-	@Test
-	public void testAddMember() {
-		Group testGroup = new Group();
-		assertTrue(testGroup.getMembers().size() == 0);
-		Person person = new Person(null, null, "person", null);
-		testGroup.addMember(person);
-		assertTrue(testGroup.getMembers().size() == 1);
-	}
-	
-	@Test
-	public void testDeleteMember() {
-		Group testGroup = new Group();
-		assertTrue(testGroup.getMembers().size() == 0);
-		Person person = new Person(null, null, "person", null);
-		testGroup.addMember(person);
-		assertTrue(testGroup.getMembers().size() == 1);
-		testGroup.deleteMember("person");
-		assertTrue(testGroup.getMembers().size() == 0);
-	}
-	
-	@Test
-	public void testAddEvent() {
-		Group testGroup = new Group();
-		assertTrue(testGroup.getGrCalendar().size() == 0);
-		Event event = new Event("event");
-		testGroup.addEvent(event);
-		assertTrue(testGroup.getGrCalendar().size() == 1);
-	}
-	
-	@Test
-	public void testDeleteEvent(){
-		Group testGroup = new Group();
-		assertTrue(testGroup.getGrCalendar().size() == 0);
-		Event event = new Event("event");
-		testGroup.addEvent(event);
-		assertTrue(testGroup.getGrCalendar().size() == 1);
-		testGroup.deleteEvent("event");
-		assertTrue(testGroup.getGrCalendar().size() == 0);
-	}
-	
-	@Test
-	public void testGetSingleMember(){
-		Group testGroup = new Group();
-		Person person = new Person(null, null, "person", null);
-		testGroup.addMember(person);
-		assertEquals(person, testGroup.getSingleMember("person"));
-	}
-
 }
