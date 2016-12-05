8a080d047b9da8a3531e9151d07e05f9cce8385a
RiddSann <nathan.vanderdonck@gmail.com>
2016-12-06 00:21:59 +0100

    Got rid of all useless tests depending on methods now taken care of by ShaCalModel. Comment yet to be taken care of, maybe tests to add. pre-release v1.0a

--- src/model/PersonTest.java
+++ src/model/PersonTest.java
@@ -1,51 +1,33 @@
 package model;
 
 import static org.junit.Assert.*;
-
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.Test;
 
 public class PersonTest {
 
-
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
 	@Test
 	public void testToString() {
-		Person p = new Person("Petit", "Martin", "NicoleEtLui", LocalDate.of(1994,9,28));
-		
-		assertEquals("Martin Petit - NicoleEtLui - 28/09/1994", p.toString());
+		Person person = new Person("name","firstName","person", LocalDate.now());
+		assertEquals("firstName name - person - " + LocalDate.now().format(formatter), person.toString());
 	}
-
+	
 	@Test
-	public void testEqualsObject() {
-		Person p = new Person("Petit", "Martin", "NicoleEtlui", LocalDate.of(1994,9,28));
+	public void testEquals(){
+		Person p0 = new Person("Petit", "Martin", "NicoleEtlui", LocalDate.of(1994,9,28));
 		Person p1 =  new Person("Petit", "Martin", "MartinP", LocalDate.of(1994,9,28));
 		Person p2 =  new Person("Petit", "Nicole", "NicoleEtlui", LocalDate.of(1994,9,28));
 		
-		assertTrue(p.equals(p));
-		assertFalse(p.equals(p1));
-		assertTrue(p.equals(p2));
+		assertTrue(p0.equals(p0));
+		assertFalse(p0.equals(p1));
+		assertTrue(p0.equals(p2));
 	}
-	@Test
-	public void testAddPersonalEvents() {
-		Person p = new Person("Petit", "Martin", "NicoleEtlui", LocalDate.of(1994,9,28));
-		assertTrue(p.getPersoClndr().size() == 1);
-		p.addPersonalEvents(new Event("MonEvent", "MaDescription", LocalDate.now(), LocalDate.now().plusDays(1)));
-		assertTrue(p.getPersoClndr().size() == 2);
-	}
-
-	@Test
-	public void testDeletePersonalEvent() {
-		Event e = new Event("MonEvent", "MaDescription", LocalDate.now(), LocalDate.now().plusDays(1));
-		Person p = new Person("Petit", "Martin", "NicoleEtlui", LocalDate.of(1994,9,28));
-		assertTrue(p.getPersoClndr().size() == 1);
-		p.addPersonalEvents(e);
-		p.deletePersonalEvent(e);
-		assertTrue(p.getPersoClndr().size() == 1);
-		p.deletePersonalEvent(e);
-		assertTrue(p.getPersoClndr().size() == 1);
-	}
-
+	
+	/*
 	@Test
 	public void testChangePermission() {
 		Person p = new Person("Petit", "Martin", "Martin1", LocalDate.of(1994,9,28));
@@ -77,5 +59,5 @@ public class PersonTest {
 		p.createGroup("monGroupe");
 		assertEquals("N[1,2]\n", p.toStringGroup(p.getGroup()));
 	}
-
+	*/
 }
