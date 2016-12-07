994dec8f6de5b4c4cf4208ef874f7499ea3f7fb0
RiddSann <nathan.vanderdonck@gmail.com>
2016-12-07 23:20:33 +0100

    Added all methods deleted from Person and Group here, and tweaked them to make them work. Tests yet to be made.

--- src/model/ShaCalModel.java
+++ src/model/ShaCalModel.java
@@ -6,91 +6,321 @@ import java.util.HashMap;
 import java.util.Observable;
 
 public class ShaCalModel extends Observable {
+	/**
+	 * The main "list" of all Persons.
+	 */
+	public HashMap<String,Person> allPersons = new HashMap<String,Person>();
+	/**
+	 * The main "list" of all Groups.
+	 */
+	public HashMap<Integer,Group> allGroups = new HashMap<Integer,Group>();
+	/**
+	 * The main "list" of all Events.
+	 */
+	public HashMap<String,ArrayList<Event>> allEvents = new HashMap<String,ArrayList<Event>>();
+
 	
-	//The "list" of Persons.
-	public static HashMap<String,Person> AllPersons = new HashMap<String,Person>();
+	//-- GETTERS & SETTERS -------------------------------------------------------------------------------
 	
-	//The "list" of Groups.
-	public static HashMap<Integer,Group> AllGroups = new HashMap<Integer,Group>();
-	
-	//The "list" of Groups.
-	public static HashMap<String,ArrayList<Event>> AllEvents = new HashMap<String,ArrayList<Event>>();
-	
-	//Adds a newly created Person to the list.
-	public static void addPersonToHashMap(Person person){
-		AllPersons.putIfAbsent(person.getUserName(), person);
+	/**
+	 * Fetches a Group.
+	 * @param object : The grId of the Group to fetch.
+	 * @return A Group.
+	 */
+	public Group getGroup(Object grId){ 		//TODO Find how to force "String" input and not "Object".
+		return allGroups.get(grId);
 	}
 	
-	//Deletes completely a Person.
-	public static void deletePersonFromHashMap(String person){
-		AllPersons.remove(person);
-		for(int i=0;i<AllGroups.size();i++){
-			AllGroups.get(AllGroups.keySet().toArray()[i]).deleteMemberFromGroup(person);
+
+	/**
+	 * Fetches a Person.
+	 * @param object : The userName of the Person to fetch.
+	 * @return A Person.
+	 */
+	public Person getPerson(Object userName){ 	//TODO Find how to force "String" input and not "Object".
+		return allPersons.get(userName);
+	}
+	
+	/**
+	 * Fetches the ArrayList of Event linked to a Person/Group.
+	 * @param creator : The creator of the ArrayList of Event as a String.
+	 * @return An ArrayList of Event.
+	 */
+	public ArrayList<Event> getEvent(Object creator){
+		return allEvents.get(creator);
+	}
+	
+
+	//-- METHODS -----------------------------------------------------------------------------------------
+	//-- GROUPS ------------------------------------------------------------------------------------------
+	
+	/**
+	 * Adds a newly created Group to the HashMap.
+	 * @param group : The Group to be added.
+	 */
+	public void addGroupToHashMap(Group group){
+		if(allGroups.get(group.getGrId())!=null){
+			System.out.println("Key \"" + group.getGrId() + "\" already in use."); //TODO Handle exception
+		}
+		allGroups.putIfAbsent(Integer.valueOf(group.getGrId()), group);
+	}
+	
+	/**
+	 * Completely removes a Group from the HashMap.
+	 * @param grId : The grId of the Group to delete.
+	 */
+	public void deleteGroupFromHashMap(int grId){
+		allGroups.remove(grId);
+		for(int i=0;i<allPersons.size();i++){
+			deleteGroupFromPerson(getPerson(allPersons.keySet().toArray()[i]).getUserName(), grId);
 		}
 	}
 	
-	//Adds a newly created Group to the list.
-	public static void addGroupToHashMap(Group group){
-		AllGroups.putIfAbsent(Integer.valueOf(group.getGrId()), group);
+	/**
+	 * Adds an userName to the list of members.
+	 * @param userName : A String used to fetch the Person behind that userName.
+	 */
+	public void addMemberToGroup(String userName, int grId){
+		getGroup(grId).getMembers().add(userName);
 	}
 	
-	//Deletes completely a Group.
-	public static void deleteGroupFromHashMap(int grId){
-		AllGroups.remove(grId);
-		for(int i=0;i<AllPersons.size();i++){
-			AllPersons.get(AllPersons.keySet().toArray()[i]).deleteGroupFromPerson(grId);
+	/**
+	 * Removes an userName from the list of members.
+	 * @param userName : A String used to fetch the Person behind that userName.
+	 */
+	public void deleteMemberFromGroup(String userName, int grId){
+		getGroup(String.valueOf(grId)).getMembers().remove(userName);
+	}
+	
+	/**
+	 * New default toString method
+	 * @return The representation of a group, based on its Id, name, isPublic, calendar and members
+	 */
+	
+	public String groupToString(Group group) {
+		int nbEvent = (getEvent(group.getGrIdString())==null) ? 0 : getEvent(group.getGrIdString()).size();
+		return "[Group] Id : \""
+				+ group.getGrId()
+				+ "\" Name : \""
+				+ group.getGrName()
+				+ "\" IsPublic : "
+				+ group.getIsPublic()
+				+ "\n" 
+				+ "Number of members : "
+				+ group.getMembers().size() 
+				+ " | Number of events : " 
+				+ nbEvent; 
+	}
+	
+	/**
+	 * Returns a String representation of a Person with its Groups and privileges.
+	 */
+	public String groupToStringMembers(Group group){
+		String str = "UserName : " + group.getGrName() + "\n";
+		for(int i=0;i<group.getMembers().size();i++){
+			str += ">[User : " + allPersons.get(group.getMembers().get(i)).getUserName() + "| Lvl : " + allPersons.get(group.getMembers().get(i)).getPermission(group.getGrId()) + "]\n";
+		}
+		return str;
+	}
+	
+	/**
+	 * Compare the equality of two person.
+	 * Two persons are equals if there userName are the same
+	 */
+	public boolean groupEquals(Group group, Object obj) {
+		if (group == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Group other = (Group) obj;
+		if(group.getGrId() == other.getGrId()) return false;
+		return true;
+	}
+	
+	//-- PERSONS -----------------------------------------------------------------------------------------
+	
+	/**
+	 * Adds a newly created Person to the HashMap.
+	 * @param person : The Person to be added.
+	 */
+	public void addPersonToHashMap(Person person){
+		if(allPersons.get(person.getUserName())!=null){
+			System.out.println("Key \"" + person.getUserName() + "\" already in use."); //TODO Handle exception
+		}
+		allPersons.putIfAbsent(person.getUserName(), person);
+	}
+	
+	/**
+	 * Completely removes a Person from the HashMap.
+	 * @param userName : The userName of the Person to delete.
+	 */
+	public void deletePersonFromHashMap(String userName){
+		allPersons.remove(userName);
+		for(int i=0;i<allGroups.size();i++){
+			deleteMemberFromGroup(userName,getGroup(allGroups.keySet().toArray()[i]).getGrId());
 		}
 	}
 	
-	//Adds an already existing Event to an already existing Group/Person.
-	public static void addEvent(String creator, Event event){
-		if(event==null){
-			AllEvents.putIfAbsent(creator, new ArrayList<Event>());
-		} else {
-			AllEvents.putIfAbsent(creator, new ArrayList<Event>());
-			AllEvents.get(creator).add(event);
+	/**
+	 * Adds a grId to the list of Groups, as a regular user.
+	 * @param grId : the Group's Id as an int.
+	 */
+	public void addGroupToPerson(String person, int grId){
+		getPerson(person).getGroup().put(grId, 0);
+	}
+	
+	/**
+	 * Removes a grId from the list of Groups.
+	 * @param grId : the Group's Id as an int.
+	 */
+	public void deleteGroupFromPerson(String person, int grId){
+		getPerson(person).getGroup().remove(grId);
+	}
+	/**
+	 * Returns a String representation of a Person.
+	 */
+	public String personToString(Person person){
+		return (person.getFirstName() + " " + person.getName() + " - " +
+				person.getUserName() + " - " + person.getbDate() );
+	}
+	
+	/**
+	 * Returns a String representation of a Person with its Groups and privileges.
+	 */
+	public String personToStringGroups(Person person){
+		String str = "UserName : " + person.getUserName() + "\n";
+		for(int i=0;i<person.getGroup().size();i++){
+			str += ">[Group : " + allGroups.get(person.getGroup().keySet().toArray()[i]).getGrName() + "| Lvl : " + person.getGroup().get(i) + "]\n";
 		}
-	}
-	
-	//Removes an Event from a Group/Person and deletes it.
-	public static void removeEvent(String grId, Event event){
-		AllEvents.get(grId).remove(event);
-	}
-	
-	//Adds a new link between an already existing Person and an already existing Group.
-	public static void addLink(String userName, int grId){
-		AllPersons.get(userName).addGroupToPerson(grId);
-		AllGroups.get(grId).addMemberToGroup(userName);
-	}
-	
-	//Remove a link between an already existing Person and an already existing Group.
-	public static void removeLink(String userName, int grId){
-		AllPersons.get(userName).deleteGroupFromPerson(grId);
-		AllGroups.get(grId).deleteMemberFromGroup(userName);
-	}
-	
-	//Reset all the HashMap
-	public static void resetAllHashMap() {
-		System.out.println("Cache vidé");
-		ShaCalModel.AllPersons.clear();
-		ShaCalModel.AllGroups.clear();
-		ShaCalModel.AllEvents.clear();
-		Group.resetCurrentId();
-	}
-	
-	//Returns a single Group
-	public static Group getGroup(Object grId){
-		return AllGroups.get(grId);
-	}
-	
-	//Returns a single Person
-	public static Person getPerson(Object userName){
-		return AllPersons.get(userName);
+		return str;
 	}
 
-	//Returns a single ArrayList of Event
-	public static ArrayList<Event> getEvent(Object creator){
-		return AllEvents.get(creator);
+	/**
+	 * Compare the equality of two person.
+	 * Two persons are equals if there userName are the same
+	 */
+	public boolean personEquals(Person person, Object obj) {
+		if (person == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (person.getUserName() == null) {
+			if (other.getUserName() != null)
+				return false;
+		} else if (!person.getUserName().equals(other.getUserName()))
+			return false;
+		return true;
 	}
+	
+	/**
+	 * this method change permissions of a user p of a group grId. 
+	 * <p>The person calling this method must belong to the same group the 
+	 * person she want to change and have a aythorization of 2 in this group<p>
+	 * 
+	 * @param userName is the userName of the person to upgrade/downgrade.
+	 * @param userLvl is the new levels of permission for the person
+	 * @param grId is the Id of the group.
+	 * @return <p>false if the userLevelof for the group grId of the person 
+	 * calling this method is under 2 or if the person doesn't belong 
+	 * to the group<p>
+	 * @return <p>true otherwise and replace former permission 
+	 * of the person p for the group grId by userlvl<p>
+	 */
+	public boolean changePermission(String admin, String userName, int grId, int userLvl) {
+		if((getPerson(userName).getGroup().get(grId)==null) || (getPerson(admin).getGroup().get(grId) != 2)){
+			return false;
+		}
+		if(getPerson(userName).getPermission(grId) != 2){
+			getPerson(userName).getGroup().put(grId,userLvl);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * this method create a new Group.
+	 * <p>the person that call this method get his list of group updated and
+	 * get highest level of authorization for this group (2).<br>
+	 * The group's members are updated with the person calling this method<p>
+	 * 
+	 * @param grName is the name of the new group
+	 * @return the identifier <code> grId <code> of the group created
+	 */
+	public int createGroup(String person, String grName){
+		Group group = new Group(grName, getPerson(person).getUserName());
+		addGroupToHashMap(group);
+		getPerson(person).getGroup().put(group.getGrId(), 2);
+		return group.getGrId();
+	}
+	
+
+	//-- EVENTS ------------------------------------------------------------------------------------------
+	/**
+	 * Adds a new Event to the Group/Person's list. 
+	 * @param creator : The grId/userName to add the Event to.
+	 * @param event : A newly created event to be associated with a creator.
+	 */
+	public void addEvent(String creator, Event event){
+		allEvents.putIfAbsent(creator, new ArrayList<Event>());
+		allEvents.get(creator).add(event);
+	}
+	
+	/**
+	 * Remove an Event from the creator's list.
+	 * @param creator : The grId/userName to remove the Event from.
+	 * @param event : The Event to be removed from the list of the creator.
+	 */
+	public void removeEvent(String creator, Event event){
+		allEvents.get(creator).remove(event);
+	}
+
+	//-- GENERAL -----------------------------------------------------------------------------------------
+	
+	/**
+	 * Adds a Person to a Group, by adding a link between them.
+	 * @param userName : The userName of the Person to link.
+	 * @param grId : The grId of the Group to link.
+	 */
+	public void addLink(String userName, int grId){
+		addGroupToPerson(userName, grId);
+		addMemberToGroup(userName, grId);
+	}
+	
+	/**
+	 * Remove a Person from a Group, by removing the link between them.
+	 * @param userName : The userName of the Person to remove.
+	 * @param grId : The grId of the Group to get removed from.
+	 */
+	public void removeLink(String userName, int grId){
+		deleteGroupFromPerson(userName, grId);
+		deleteMemberFromGroup(userName, grId);
+	}
+	
+	/**
+	 * Reset all the HashMaps and the grId, to start full fresh.
+	 */
+	public void resetAllHashMap() {
+		allPersons.clear();
+		allGroups.clear();
+		allEvents.clear();
+		Group.resetCurrentId();
+		System.out.println("Cache vidé");
+	}
+	
+	//-- UNCHECKED ---------------------------------------------------------------------------------------
+
+	/** [ TODO ]
+	 * Alerts a member.
+	 * @param Members An ArrayList of Persons to alert.
+	 */
+	public boolean alertMembers(String Members){
+		return false;
+	}
+	
 }
 
--- src/model/ShaCalModelTest.java
+++ src/model/ShaCalModelTest.java
@@ -7,101 +7,117 @@ import java.time.LocalDate;
 import org.junit.Test;
 
 public class ShaCalModelTest {
-	
+	ShaCalModel model = new ShaCalModel();
 	@Test
 	public void addPersonToHashMap() {
-		ShaCalModel.resetAllHashMap();
-		assertTrue(ShaCalModel.AllPersons.size()==0);
+		model.resetAllHashMap();
+		assertTrue(model.allPersons.size()==0);
 		Person person = new Person("name","firstName","person", LocalDate.now());
-		assertTrue(ShaCalModel.AllPersons.size()==1);
+		model.addPersonToHashMap(person);
+		assertTrue(model.allPersons.size()==1);
 	}
 	
 	@Test
 	public void deletePersonFromHashMap() {
-		ShaCalModel.resetAllHashMap();
+		model.resetAllHashMap();
 		Person person = new Person("name","firstName","person", LocalDate.now());
-		assertTrue(ShaCalModel.AllPersons.size()==1);
-		ShaCalModel.deletePersonFromHashMap(person.getUserName());
-		assertTrue(ShaCalModel.AllPersons.size()==0);
+		model.addPersonToHashMap(person);
+		assertTrue(model.allPersons.size()==1);
+		int gr0Id = model.createGroup(person.getUserName(), "group0");
+		System.out.println("seges" + model.allGroups);
+		assertTrue(model.getGroup(gr0Id).getMembers().size()==1);
+		model.deletePersonFromHashMap(person.getUserName());
+		assertTrue(model.allPersons.size()==0);
+		assertTrue(model.getGroup(gr0Id).getMembers().size()==0);
 		
 	}
 
 	@Test
 	public void addGroupToHashMap() {
-		ShaCalModel.resetAllHashMap();
-		assertTrue(ShaCalModel.AllGroups.size()==0);
+		model.resetAllHashMap();
+		assertTrue(model.allGroups.size()==0);
 		Group group = new Group("group");
-		assertTrue(ShaCalModel.AllGroups.size()==1);
+		model.addGroupToHashMap(group);
+		assertTrue(model.allGroups.size()==1);
 	}
 	
 	@Test
 	public void deleteGroupFromHashMap() {
-		ShaCalModel.resetAllHashMap();
-		Group group = new Group("group1");
-		assertTrue(ShaCalModel.AllGroups.size()==1);
-		ShaCalModel.deleteGroupFromHashMap(group.getGrId());
-		assertTrue(ShaCalModel.AllGroups.size()==0);
+		model.resetAllHashMap();
+		Group group = new Group("group");
+		model.addGroupToHashMap(group);
+		assertTrue(model.allGroups.size()==1);
+		model.deleteGroupFromHashMap(group.getGrId());
+		assertTrue(model.allGroups.size()==0);
 		
 	}
 	
 	@Test
 	public void addEvent() {
-		ShaCalModel.resetAllHashMap();
-		assertTrue(ShaCalModel.AllEvents.size()==0);
+		model.resetAllHashMap();
+		assertTrue(model.allEvents.size()==0);
 		Group group = new Group("group");
-		assertTrue(ShaCalModel.AllEvents.size()==1); //Set to 1 because of one entity already having a link to this HashMap.
+		model.addGroupToHashMap(group);
 		Event event = new Event("event");
-		assertTrue(ShaCalModel.getEvent(group.getGrIdString()).size()==0);
-		ShaCalModel.addEvent(group.getGrIdString(),event);
-		assertTrue(ShaCalModel.getEvent(group.getGrIdString()).size()==1);
+		assertTrue(model.getEvent(group.getGrIdString())==null);
+		model.addEvent(group.getGrIdString(),event);
+		assertTrue(model.getEvent(group.getGrIdString()).size()==1);
 	}
 	
 	@Test
 	public void removeEvent() {
-		ShaCalModel.resetAllHashMap();
+		model.resetAllHashMap();
 		Group group = new Group("group");
 		Event event = new Event("event");
-		ShaCalModel.addEvent(group.getGrIdString(),event);
-		assertTrue(ShaCalModel.getEvent(group.getGrIdString()).size()==1);
-		ShaCalModel.removeEvent(group.getGrIdString(),event);
-		assertTrue(ShaCalModel.getEvent(group.getGrIdString()).size()==0);
+		model.addEvent(group.getGrIdString(),event);
+		assertTrue(model.getEvent(group.getGrIdString()).size()==1);
+		model.removeEvent(group.getGrIdString(),event);
+		assertTrue(model.getEvent(group.getGrIdString()).size()==0);
 	}
 	
 	@Test
 	public void addLink() {
-		ShaCalModel.resetAllHashMap();
+		model.resetAllHashMap();
 		Group group = new Group("group");
+		model.addGroupToHashMap(group);
 		Person person = new Person("name","firstName","person", LocalDate.now());
-		assertTrue(ShaCalModel.getGroup(ShaCalModel.AllGroups.keySet().toArray()[0]).getMembers().size()==0);
-		assertTrue(ShaCalModel.getPerson(ShaCalModel.AllPersons.keySet().toArray()[0]).getGroup().size()==0);
-		ShaCalModel.addLink(person.getUserName(), group.getGrId());
-		assertTrue(ShaCalModel.getGroup(ShaCalModel.AllGroups.keySet().toArray()[0]).getMembers().size()==1);
-		assertTrue(ShaCalModel.getPerson(ShaCalModel.AllPersons.keySet().toArray()[0]).getGroup().size()==1);
+		model.addPersonToHashMap(person);
+		assertTrue(model.getGroup(model.allGroups.keySet().toArray()[0]).getMembers().size()==0);
+		assertTrue(model.getPerson(model.allPersons.keySet().toArray()[0]).getGroup().size()==0);
+		model.addLink(person.getUserName(), group.getGrId());
+		assertTrue(model.getGroup(model.allGroups.keySet().toArray()[0]).getMembers().size()==1);
+		assertTrue(model.getPerson(model.allPersons.keySet().toArray()[0]).getGroup().size()==1);
 	}
 	
 	@Test
 	public void removeLink() {
-		ShaCalModel.resetAllHashMap();
+		model.resetAllHashMap();
 		Group group = new Group("group");
+		model.addGroupToHashMap(group);
 		Person person = new Person("name","firstName","person", LocalDate.now());
-		ShaCalModel.addLink(person.getUserName(), group.getGrId());
-		assertTrue(ShaCalModel.getGroup(ShaCalModel.AllGroups.keySet().toArray()[0]).getMembers().size()==1);
-		assertTrue(ShaCalModel.getPerson(ShaCalModel.AllPersons.keySet().toArray()[0]).getGroup().size()==1);
-		ShaCalModel.removeLink(person.getUserName(), group.getGrId());
-		assertTrue(ShaCalModel.getGroup(ShaCalModel.AllGroups.keySet().toArray()[0]).getMembers().size()==0);
-		assertTrue(ShaCalModel.getPerson(ShaCalModel.AllPersons.keySet().toArray()[0]).getGroup().size()==0);
+		model.addPersonToHashMap(person);
+		model.addLink(person.getUserName(), group.getGrId());
+		assertTrue(model.getGroup(model.allGroups.keySet().toArray()[0]).getMembers().size()==1);
+		assertTrue(model.getPerson(model.allPersons.keySet().toArray()[0]).getGroup().size()==1);
+		model.removeLink(person.getUserName(), group.getGrId());
+		assertTrue(model.getGroup(model.allGroups.keySet().toArray()[0]).getMembers().size()==0);
+		assertTrue(model.getPerson(model.allPersons.keySet().toArray()[0]).getGroup().size()==0);
 	}
 	
 	@Test
 	public void resetAllHashMap() {
-		ShaCalModel.resetAllHashMap();
-		assertTrue(ShaCalModel.AllPersons.size()==0);
-		assertTrue(ShaCalModel.AllGroups.size()==0);
-		assertTrue(ShaCalModel.AllEvents.size()==0);
+		model.resetAllHashMap();
+		assertTrue(model.allPersons.size()==0);
+		assertTrue(model.allGroups.size()==0);
+		assertTrue(model.allEvents.size()==0);
 		Person person = new Person("name","firstName","person", LocalDate.now());
-		Group group= new Group();
-		assertTrue(ShaCalModel.AllPersons.size()==1);
-		assertTrue(ShaCalModel.AllGroups.size()==1);
-		assertTrue(ShaCalModel.AllEvents.size()==2); //Set to 2 because of two entities already having a link to this HashMap.
+		Group group = new Group();
+		Event event = new Event("event");
+		model.addPersonToHashMap(person);
+		model.addGroupToHashMap(group);
+		model.addEvent("person", event);
+		assertTrue(model.allPersons.size()==1);
+		assertTrue(model.allGroups.size()==1);
+		assertTrue(model.allEvents.size()==1); //Set to 2 because of two entities already having a link to this HashMap.
 	}
 }
