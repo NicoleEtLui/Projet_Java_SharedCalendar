9498b3223fcc16a06e303896ba52d71dffcbaf84
NicoleEtLui <petit.martin125@gmail.com>
2016-12-21 16:32:57 +0100

    encore du void

--- src/test/SharedCalendar.java
+++ src/test/SharedCalendar.java
@@ -8,10 +8,10 @@ public class SharedCalendar {
 	public SharedCalendar(){
 		//création du modèle
 		ShaCalModel shacalmod = new ShaCalModel();
+		
 		//création des controlleurs un pour chaque vue
 		//chaque controlleur doit avoir une référence vers le modèle pour pouvoir le commander
 		ShaCalController shacalcontrolConsol = new ShaCalController(shacalmod);
-		
 		//Création des vues
 		//chaque vue doit avoir une référence a son controleur et avoir une référence vers le modèle pour pouvoir l'observer
 		ShaCalViewConsol console = new ShaCalViewConsol(shacalmod, shacalcontrolConsol);
